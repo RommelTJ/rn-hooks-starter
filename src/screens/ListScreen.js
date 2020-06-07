0b0144c822831cfe0587923fd1ffc2c753a65129
Rommel Rico <me@rommelrico.com>
2020-06-07 12:20:09 -0700

    :sparkles: List: Fixing key issue method 2

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,19 +4,20 @@ import { FlatList, View, Text, StyleSheet } from 'react-native';
 const ListScreen = () => {
 
   const friends = [
-    {name: "friend1", key: "1"},
-    {name: "friend2", key: "2"},
-    {name: "friend3", key: "3"},
-    {name: "friend4", key: "4"},
-    {name: "friend5", key: "5"},
-    {name: "friend6", key: "6"},
-    {name: "friend7", key: "7"},
-    {name: "friend8", key: "8"},
-    {name: "friend9", key: "9"},
+    {name: "friend1"},
+    {name: "friend2"},
+    {name: "friend3"},
+    {name: "friend4"},
+    {name: "friend5"},
+    {name: "friend6"},
+    {name: "friend7"},
+    {name: "friend8"},
+    {name: "friend9"},
   ]
 
   return (
     <FlatList
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({item}) => {
         return <Text>{item.name}</Text>;
