535064c12905191e9d94e3ae9c051baa150be41c
Rommel Rico <me@rommelrico.com>
2020-06-07 12:08:25 -0700

    :sparkles: Lists: The FlatList element

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
 
 const ListScreen = () => {
   return (
