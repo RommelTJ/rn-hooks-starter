cea4cca2ff25c8f73bc1a3b442a2a704cedf9f15
Rommel Rico <me@rommelrico.com>
2020-06-11 07:11:04 -0700

    :sparkles: State: Correctly updating state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 const CounterScreen = () => {
-  // TODO: Fix this
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
+
+  const onIncrease = () => setCounter(counter+1);
+  const onDecrease = () => setCounter(counter-1);
 
   return (
     <View>
-      <Button title={"Increase"} onPress={() => { counter++; }} />
-      <Button title={"Decrease"} onPress={() => { counter--; }} />
+      <Button title={"Increase"} onPress={onIncrease} />
+      <Button title={"Decrease"} onPress={onDecrease} />
       <Text>Current Count: {counter}</Text>
     </View>
   );
