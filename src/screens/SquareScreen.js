be3592495dafd6bc86081e1f3e6dae87076ce4bb
Rommel Rico <me@rommelrico.com>
2020-06-12 07:18:28 -0700

    :tada: State: SquareScreen

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SquareScreen = (props) => {
+  return <View><Text>Hello Square Screen</Text></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
