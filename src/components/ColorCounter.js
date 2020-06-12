1e9d95535ef3f491772c4bbb9b609d85c917cd17
Rommel Rico <me@rommelrico.com>
2020-06-12 07:20:19 -0700

    :tada: State: Color Counter

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ColorCounter = (props) => {
+  return <View><Text>Hello Color Counter</Text></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
