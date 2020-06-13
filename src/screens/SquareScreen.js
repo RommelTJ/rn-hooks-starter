f8996a262bcb93d3e04c969a64c5b9469a0946f8
Rommel Rico <me@rommelrico.com>
2020-06-13 13:00:31 -0700

    :sparkles: State: Tying State Values Together

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,21 +3,23 @@ import { View, StyleSheet } from "react-native";
 
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
   const onIncrease = (color) => {
-    if (color === "Red") setRed(red + 1);
-    if (color === "Green") setGreen(green + 1);
-    if (color === "Blue") setBlue(blue + 1);
+    if (color === "Red") setRed(red + COLOR_INCREMENT);
+    if (color === "Green") setGreen(green + COLOR_INCREMENT);
+    if (color === "Blue") setBlue(blue + COLOR_INCREMENT);
   }
 
   const onDecrease = (color) => {
-    if (color === "Red") setRed(red - 1);
-    if (color === "Green") setGreen(green - 1);
-    if (color === "Blue") setBlue(blue - 1);
+    if (color === "Red") setRed(red - COLOR_INCREMENT);
+    if (color === "Green") setGreen(green - COLOR_INCREMENT);
+    if (color === "Blue") setBlue(blue - COLOR_INCREMENT);
   }
 
   return (
@@ -25,6 +27,7 @@ const SquareScreen = () => {
       <ColorCounter color={"Red"} onIncrease={() => onIncrease("Red")} onDecrease={() => onDecrease("Red")} />
       <ColorCounter color={"Green"} onIncrease={() => onIncrease("Green")} onDecrease={() => onDecrease("Green")} />
       <ColorCounter color={"Blue"} onIncrease={() => onIncrease("Blue")} onDecrease={() => onDecrease("Blue")} />
+      <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
     </View>
   );
 };
