9bdc8a90d9a576a207c171d38f549bf97d3b90d6
Rommel Rico <me@rommelrico.com>
2020-06-14 08:54:24 -0700

    :sparkles: State: Validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,10 +8,17 @@ const COLOR_INCREMENT = 15;
 const INITIAL_STATE = { red: 0, green: 0, blue: 0};
 const reducer = (state, action) => {
   switch (action.colorToChange) {
-    case "red":   return { ...state, red: state.red + action.amount };
-    case "green": return { ...state, green: state.green + action.amount };
-    case "blue":  return { ...state, blue: state.blue + action.amount };
-    default:      return state;
+    case "red":
+      if (state.red + action.amount > 255 || state.red + action.amount < 0) return state;
+      return { ...state, red: state.red + action.amount };
+    case "green":
+      if (state.green + action.amount > 255 || state.green + action.amount < 0) return state;
+      return { ...state, green: state.green + action.amount };
+    case "blue":
+      if (state.blue + action.amount > 255 || state.blue + action.amount < 0) return state;
+      return { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
   }
 };
 
