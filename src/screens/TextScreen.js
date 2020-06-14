c168f835bfc0b122e68bf8e568e8fbc1c4db45ed
Rommel Rico <me@rommelrico.com>
2020-06-14 09:37:18 -0700

    :sparkles: State: Exercise

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,19 @@ import React, { useState } from "react"
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <View>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={(text) => setName(text)}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
       />
-      <Text>User Typed: {name}</Text>
+      { password.length <= 5 ? <Text>Password must be longer than 5 characters</Text> : undefined }
     </View>
   );
 }
