2b682d6c43f420f4ec98ca2cff252bd5f93d0c5b
Rommel Rico <me@rommelrico.com>
2020-06-14 08:43:38 -0700

    :sparkles: State: Creating a reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { View, StyleSheet } from "react-native";
 
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 15;
 
+const INITIAL_STATE = { red: 0, green: 0, blue: 0};
+const reducer = (state, action) => {
+  switch (action.colorToChange) {
+    case "red":   return { ...state, red: state.red + action.amount };
+    case "green": return { ...state, green: state.green + action.amount };
+    case "blue":  return { ...state, blue: state.blue + action.amount };
+    default:      return state;
+  }
+};
+
 const SquareScreen = () => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  console.log(state);
+
   return (
     <View>
       <ColorCounter color={"Red"} onIncrease={() => {}} onDecrease={() => {}} />
