a453337b7f939c31934f16024e74d9dfd4858e7d
Rommel Rico <me@rommelrico.com>
2020-06-14 08:50:08 -0700

    :sparkles: State: Applying State with a Reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -21,10 +21,22 @@ const SquareScreen = () => {
 
   return (
     <View>
-      <ColorCounter color={"Red"} onIncrease={() => {}} onDecrease={() => {}} />
-      <ColorCounter color={"Green"} onIncrease={() => {}} onDecrease={() => {}} />
-      <ColorCounter color={"Blue"} onIncrease={() => {}} onDecrease={() => {}} />
-      <View style={{ height: 150, width: 150, backgroundColor: `rgb(${"red"}, ${"green"}, ${"blue"})` }} />
+      <ColorCounter
+        color={"Red"}
+        onIncrease={() => dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT })}
+      />
+      <ColorCounter
+        color={"Green"}
+        onIncrease={() => dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT })}
+      />
+      <ColorCounter
+        color={"Blue"}
+        onIncrease={() => dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })}
+        onDecrease={() => dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT })}
+      />
+      <View style={{ height: 150, width: 150, backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})` }} />
     </View>
   );
 };
